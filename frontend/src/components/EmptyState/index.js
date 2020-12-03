b7192d484ba114db9422a7b824e180eb6b4843ab
Jacob <macintoshhd@Jacobs-MacBook-Pro.local>
2020-12-03 22:40:15 +0700

    feat: add frontend; docs: update Readme.md

---
+++ frontend/src/components/EmptyState/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function EmptyState(props){
+
+    return(
+        <div>
+            <center>
+                <h4>Belum ada item yang dipilih</h4>
+                <p>Klik salah satu item di List Movies</p>
+            </center>
+        </div>
+    )
+}
